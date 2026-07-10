fd6ce8a09395392e5058405ed13c181d4e156f35
tatiman2000 <98978008+tatiman2000@users.noreply.github.com>
2026-07-10 21:30:56 +0200

    pequeños cambios al DASH y comentarios para organización

--- scripts/characters/main_doctor.gd
+++ scripts/characters/main_doctor.gd
@@ -6,14 +6,14 @@ const JUMP_VELOCITY = -400.0
 
 const DASH_SPEED = 800.0       
 const DASH_DURATION = 0.2     
-const DASH_COOLDOWN = 1.0     
+const DASH_COOLDOWN = 0.75     
 
 var walk = false
 var lantern = false
 var is_attacking = false
 
 var is_dashing = false         
-var is_dash_ending = false    
+var end_dashing = false    
 var can_dash = true           
 var dash_direction = 1.0       
 
@@ -22,24 +22,39 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor() and not is_dashing:
 		velocity += get_gravity() * delta
 
+	#EL VERDADERO DASH
 	if is_dashing:
 		velocity.x = dash_direction * DASH_SPEED
 		velocity.y = 0 
 		move_and_slide()
 		return
-
-	if is_dash_ending or is_attacking:
+		
+	if end_dashing:
+		velocity.x = velocity.x * 0.9
+		move_and_slide()
+		return
+		
+	#EL VERDADERO ATAQUE
+	if is_attacking:
 		velocity.x = 0
 		move_and_slide()
 		return
+		
+
 
 	if Input.is_action_just_pressed("ui_dash") and can_dash:
 		start_dash()
 		return 
-
+	
+	"""
+	"ATAQUE":
+		Reproduce animaciones
+		Espera
+		Y cambia bools de valor
+		(El verdadero ATAQUE está al principio del physics void)
+	"""
 	if Input.is_action_just_pressed("ui_attack") and is_on_floor():
 		is_attacking = true
-		velocity.x = 0 
 		if not lantern:
 			lantern = true
 			animsprite.play("attack")
@@ -50,7 +65,8 @@ func _physics_process(delta: float) -> void:
 		await animsprite.animation_finished
 		is_attacking = false
 		return 
-
+	
+	#EL VERDADERO MOVIMIENTO
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
@@ -69,6 +85,7 @@ func _physics_process(delta: float) -> void:
 	if direction == 0:
 		dash_direction = -1.0 if animsprite.flip_h else 1.0
 
+	#SALTO
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -85,6 +102,14 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+
+"""
+DASH:
+	Reproduce animaciones
+	Espera
+	Y cambia bools de valor
+	(El verdadero DASH está al principio del physics void)
+"""
 func start_dash() -> void:
 	is_dashing = true
 	can_dash = false
@@ -94,8 +119,7 @@ func start_dash() -> void:
 	await get_tree().create_timer(DASH_DURATION).timeout
 	is_dashing = false
 	
-	is_dash_ending = true
-	velocity.x = 0 
+	end_dashing = true
 	
 	if lantern:
 		animsprite.play("stop2")
@@ -104,7 +128,7 @@ func start_dash() -> void:
 		
 	await animsprite.animation_finished 
 	
-	is_dash_ending = false 
+	end_dashing = false 
 
 	await get_tree().create_timer(DASH_COOLDOWN).timeout
 	can_dash = true
